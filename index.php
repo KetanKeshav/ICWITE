52523d52d8b85f6e43bbbb50ec5c44ce5c7275f9
anzar00 <peerzada.anzar@gmail.com>
2022-09-12 01:21:49 +0530

    Contact Form

--- index.php
+++ index.php
@@ -1287,22 +1287,22 @@ tickets
     <br><br><br>
     <!--   Contact Section
                ============================-->
-    <!-- <section class="pb100">
+    <section class="pb100">
         <div class="container">
             <div class="section_title mb50">
                 <h3 class="title">
                     Contact Us
                 </h3>
             </div>
-            <div class="row no-gutters">
+            <!-- <div class="row no-gutters">
 			<div class="col-lg-6 venue-map">
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15531.51793096121!2d77.59632649781835!3d13.295468100605275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec91545e88c87ae9!2sGITAM%20institute%20of%20technology%20and%20management%2C%20Bangalore!5e0!3m2!1sen!2sin!4v1577970934467!5m2!1sen!2sin" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 			</div>
-		</div>
+		    </div> -->
             <div class="form">
                 <div id="sendmessage"></div>
                 <div id="errormessage"></div>
-                <form action="" method="post" role="form" class="contactForm">
+                <form action="./forms/mail.php" method="post" role="form" class="contactForm">
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
@@ -1322,12 +1322,12 @@ tickets
                         <div class="validation"></div>
                     </div>
                     <div class="text-center">
-                        <button type="submit" class="btn btn-primary btn-rounded mt30">Send Message</button>
+                        <input type="submit" class="btn btn-primary btn-rounded mt30" value ="Send Message">
                     </div>
                 </form>
             </div>
         </div>
-    </section> -->
+    </section>
     <!-- #contact -->
 
     <?php include 'includes/footer.php'; ?>
