40aecad9289f7cd41101edb79ff7fcf458d95f21
anzar00 <peerzada.anzar@gmail.com>
2022-10-16 00:47:28 +0530

    venue

--- venue.php
+++ venue.php
@@ -45,7 +45,7 @@
             <br><br>
             <div class="row no-gutters">
                 <div class="col-lg-12 col-sm-6 col-md-6 venue-map">
-                <h4>Dierctions to reach</h4>                
+                <h4>Directions to reach</h4>                
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.2168296543355!2d77.56495361482256!3d13.021859690822915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17d757d11bfb%3A0x3fc1cd64d345c13f!2sIndian%20Institute%20of%20Science!5e0!3m2!1sen!2sin!4v1662287370804!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
